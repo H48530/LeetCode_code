7149819af3495675a7a716465a9a0c667b43c164
H48530 <485301267@qq.com>
2021-03-18 18:44:42 +0800

    链表的回文结构

---
+++ 链表的回文结构.java
@@ -0,0 +1,23 @@
+package nowcode4;
+
+import leetcode_p203.ListNode;
+
+/**
+ *对于一个链表，请设计一个时间复杂度为O(n),额外空间复杂度为O(1)的算法，判断其是否为回文结构。
+ * 给定一个链表的头指针A，请返回一个bool值，代表其是否为回文结构。保证链表长度小于等于900。
+ * 测试样例：
+ * 1->2->2->1
+ * 返回：true
+ */
+
+
+class PalindromeList {
+    public boolean chkPalindrome(ListNode A) {
+        // write code here
+
+        return false;
+    }
+}
+
+public class 链表的回文结构 {
+}
