4424349a1c6d02669eaf9f00eeddc380f7c5c594
H48530 <485301267@qq.com>
2021-03-21 14:19:31 +0800

    环形链表（找到入环第一个点）

---
+++ 环形链表2.java
@@ -0,0 +1,47 @@
+package Day0321;
+
+import java.util.List;
+
+/**
+ * 142. 环形链表 II   https://leetcode-cn.com/problems/linked-list-cycle-ii/description/
+ * 给定一个链表，返回链表开始入环的第一个节点。 如果链表无环，则返回 null。
+ * 为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。
+ * 如果 pos 是 -1，则在该链表中没有环。注意，pos 仅仅是用于标识环的情况，并不会作为参数传递到函数中。
+ */
+
+
+class Solution1 {
+    public ListNode detectCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return null;
+        }
+        boolean flg = false;
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast.next != null) {
+            fast = fast.next;
+            if (fast.next == null) {
+                return null;
+            }
+            fast = fast.next;
+            slow = slow.next;
+            if (fast.equals(slow)) {
+                flg = true;
+                break;
+            }
+        }
+        if (flg) {
+            slow = head;
+            while (!slow.equals(fast) && fast != null) {
+                slow = slow.next;
+                fast = fast.next;
+            }
+            return slow;
+        }else {
+            return null;
+        }
+    }
+}
+
+public class 环形链表2 {
+}
