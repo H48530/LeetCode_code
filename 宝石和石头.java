9670ef7b674b404640374b951ddbbea0e1c352c0
H48530 <485301267@qq.com>
2021-04-10 22:55:45 +0800

    宝石和石头

---
+++ 宝石和石头.java
@@ -0,0 +1,28 @@
+package Day0410;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ * 771. 宝石与石头           https://leetcode-cn.com/problems/jewels-and-stones/submissions/
+ * 给定字符串J 代表石头中宝石的类型，和字符串 S代表你拥有的石头。 S 中每个字符代表了一种你拥有的石头的类型，你想知道你拥有的石头中有多少是宝石。
+ * J 中的字母不重复，J 和 S中的所有字符都是字母。字母区分大小写，因此"a"和"A"是不同类型的石头。
+ */
+class Solution1 {
+    public int numJewelsInStones(String jewels, String stones) {
+        Set<Character> set = new TreeSet<>();
+        for (char c : jewels.toCharArray()) {
+            set.add(c);
+        }
+        int count = 0;
+        for (char c : stones.toCharArray()) {
+            if (set.contains(c)) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
+
+public class 宝石和石头 {
+}
