48cc67989589a15e41e8b1fe1adc5689dcda6bc3
H48530 <485301267@qq.com>
2021-05-22 18:23:17 +0800

    奇数位为奇数偶数位为偶数

---
+++ 每日一题/Day0519/奇数位为奇数偶数位为偶数.java
@@ -0,0 +1,31 @@
+package Day0519;
+
+/**
+ *
+ */
+public class 奇数位为奇数偶数位为偶数 {
+
+    public void oddInOddEvenInEven(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        int even = 0;
+        int odd = 1;
+        int end = arr.length - 1;
+        while (even <= end && odd <= end) {
+            if (arr[end] % 2 == 0) {
+                swap(arr, end, even);
+                even += 2;
+            } else {
+                swap(arr, end, odd);
+                odd += 2;
+            }
+        }
+    }
+
+    public void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
