9f34d2dc79c93ea685d15cb851ea367ababd486f
H48530 <485301267@qq.com>
2021-08-03 22:13:51 +0800

    反转链表

---
+++ NowCoderPractice/链表/反转链表.java
@@ -0,0 +1,22 @@
+/*https://www.nowcoder.com/practice/75e878df47f24fdc9dc3e400ec6058ca
+public class ListNode {
+    int val;
+    ListNode next = null;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}*/
+public class Solution {
+    public ListNode ReverseList(ListNode head) {
+        ListNode cur = head;
+        ListNode pre = null;
+        while(cur != null){
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
+    }
+}
